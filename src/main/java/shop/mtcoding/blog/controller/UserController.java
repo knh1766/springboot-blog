474fbba8367f7d3f581e01c9f343cdeb731bb5fe
knh1766 <knh1766@naver.com>
2023-07-28 10:58:32 +0900

    뷰완료

--- src/main/java/shop/mtcoding/blog/controller/UserController.java
+++ src/main/java/shop/mtcoding/blog/controller/UserController.java
@@ -1,10 +1,23 @@
 package shop.mtcoding.blog.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+
+    @PostMapping("/join")
+    public void join(String username, int password, String email, HttpServletResponse response) throws IOException {
+        System.out.println("username : " + username);
+        System.out.println("password :" + password);
+        System.out.println("email :" + email);
+
+    }
 
     @GetMapping("/joinForm")
     public String joinForm() {
