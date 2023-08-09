43d675495fb10b95a8abfbf0f762e9d4ff5b67ac
knh1766 <knh1766@naver.com>
2023-08-09 11:51:02 +0900

    댓글삭제완료

--- src/main/java/shop/mtcoding/blog/controller/ReplyController.java
+++ src/main/java/shop/mtcoding/blog/controller/ReplyController.java
@@ -4,41 +4,67 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import shop.mtcoding.blog.dto.ReplyWriteDTO;
+import shop.mtcoding.blog.model.Reply;
 import shop.mtcoding.blog.model.User;
 import shop.mtcoding.blog.repository.ReplyRepository;
 
 @Controller
 public class ReplyController {
+
     @Autowired
     private HttpSession session;
 
     @Autowired
     private ReplyRepository replyRepository;
 
+    @PostMapping("/reply/{id}/delete")
+    public String delete(@PathVariable Integer id, Integer boardId) {
+        // 유효성 검사
+        if (boardId == null) {
+            return "redirect:/40x";
+        }
+
+        // 인증체크
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if (sessionUser == null) {
+            return "redirect:/loginForm";
+        }
+
+        // 권한체크
+        Reply reply = replyRepository.findById(id);
+        if (reply.getUser().getId() != sessionUser.getId()) {
+            return "redirect:/40x"; // 403
+        }
+
+        // 핵심로직
+        replyRepository.deleteById(id);
+
+        return "redirect:/board/" + boardId;
+    }
+
     @PostMapping("/reply/save")
     public String save(ReplyWriteDTO replyWriteDTO) {
-        // comment boardid유효성검사
-
-        if (replyWriteDTO.getComment() == null) {
+        // comment, boardId 유효성 검사
+        if (replyWriteDTO.getBoardId() == null) {
             return "redirect:/40x";
         }
         if (replyWriteDTO.getComment() == null || replyWriteDTO.getComment().isEmpty()) {
             return "redirect:/40x";
         }
 
-        // 인증검사
+        // 인증 검사
         User sessionUser = (User) session.getAttribute("sessionUser");
         if (sessionUser == null) {
             return "redirect:/loginForm";
         }
 
-        // 댓글쓰기
+        // 댓글 쓰기
         replyRepository.save(replyWriteDTO, sessionUser.getId());
 
         return "redirect:/board/" + replyWriteDTO.getBoardId();
     }
-
 }
--- src/main/java/shop/mtcoding/blog/repository/ReplyRepository.java
+++ src/main/java/shop/mtcoding/blog/repository/ReplyRepository.java
@@ -1,5 +1,7 @@
 package shop.mtcoding.blog.repository;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
@@ -8,12 +10,25 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import shop.mtcoding.blog.dto.ReplyWriteDTO;
+import shop.mtcoding.blog.model.Reply;
 
 @Repository
 public class ReplyRepository {
 
     @Autowired
     private EntityManager em;
+
+    public Reply findById(int id) {
+        Query query = em.createNativeQuery("select * from reply_tb where id = :id", Reply.class);
+        query.setParameter("id", id);
+        return (Reply) query.getSingleResult();
+    }
+
+    public List<Reply> findByBoardId(Integer boardId) {
+        Query query = em.createNativeQuery("select * from reply_tb where board_id = :boardId", Reply.class);
+        query.setParameter("boardId", boardId);
+        return query.getResultList();
+    }
 
     @Transactional
     public void save(ReplyWriteDTO replyWriteDTO, Integer userId) {
@@ -24,6 +39,15 @@ public class ReplyRepository {
         query.setParameter("boardId", replyWriteDTO.getBoardId());
         query.setParameter("comment", replyWriteDTO.getComment());
         query.setParameter("userId", userId);
+        query.executeUpdate();
+    }
+
+    @Transactional
+    public void deleteById(Integer id) {
+        Query query = em
+                .createNativeQuery(
+                        "delete from reply_tb where id = :id");
+        query.setParameter("id", id);
         query.executeUpdate();
     }
 
